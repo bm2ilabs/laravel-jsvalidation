3ad9108db50fb9e477a1040a4254ca02000cb830
Boukraa Mohamed <bm2ilabs@gmail.com>
2017-01-11 11:29:33 +0100

    fix the issue of Undefined index: _jsvalidation_validate_all

--- src/Remote/Resolver.php
+++ src/Remote/Resolver.php
@@ -94,7 +94,7 @@ class Resolver
     {
         return function ($attribute, $value, $parameters, BaseValidator $validator) {
             $data = $validator->getData();
-            $validateAll = $data[$attribute.'_validate_all'] === 'true' ? true : false;
+            $validateAll = isset($data[$attribute.'_validate_all']) ? true : false;
             $remoteValidator = new Validator($validator);
             $remoteValidator->setValidateAll($validateAll);
             $remoteValidator->validate($attribute, $value, $parameters);
